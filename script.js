d21b71c943dcfe27ab1e97abddb2423151847f5d
plainp <tarantoj04@gmail.com>
2024-01-04 18:17:18 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "assets/avatar-light.png"),
+      img.setAttribute("alt", "Avatar Light Mode");
+  } else {
+    img.setAttribute("src", "assets/avatar.png"),
+      img.setAttribute("alt", "Avatar Dark Mode");
+  }
+}
